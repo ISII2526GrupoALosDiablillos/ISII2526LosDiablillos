ff7afc1939a22e7bc742e8c19a5b34ca35493234
ÍKER VALÍA GARCÍA <Iker.Valia@alu.uclm.es>
2025-10-04 19:16:46 +0200

    #6 Añado unos cambios que faltaban por hacer en la clase herramienta

--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -5,13 +5,20 @@
         public int compraItems {  get; set; }
         [Key]
         public int id {  get; set; }
+        [StringLength(100, ErrorMessage = "El nombre no puede tener mas de 100 caracteres")]
         public int itemsReparación {  get; set; }
         public string material { get; set; }
+        [StringLength(100, ErrorMessage ="El nombre no puede tener mas de 100 caracteres")]
+
         public string nombre {  get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, float.MaxValue, ErrorMessage = "Precio mínimo es 0.5")]
         public int OfertaItems {  get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5,float.MaxValue, ErrorMessage = "Precio mínimo es 0.5")]
         public int precio {  get; set; }
         public int tiempoReparacion {  get; set; }
 
-
+        public Fabricante Fabricante { get; set; }
     }
 }
