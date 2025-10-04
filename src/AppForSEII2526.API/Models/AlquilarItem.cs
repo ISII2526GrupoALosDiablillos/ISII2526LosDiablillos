00a70da9a0d482197c8fc47bb8d8e9bac6cc5830
ÍKER VALÍA GARCÍA <Iker.Valia@alu.uclm.es>
2025-10-04 20:07:49 +0200

    #12 Creo la clase alquilar y alquilarItem solo falta implentar los metodos de pago

---
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class AlquilarItem
+    {
+        public int idAlquiler {  get; set; }
+        public Herramienta herramienta { get; set; }
+        public int idHerramienta {  set; get; }
+        public int precio {  set; get; }
+        public int cantidad {  set; get; }
+
+
+    }
+}
