df7fe80df5a60cfe9ae7667e59ec335001028d4f
Gonzalo <Gonzalo.Ortiz@alu.uclm.es>
2025-10-04 19:29:59 +0200

    #9

---
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Compra
+{
+	public Compra()
+	{
+		public string apellidoCliente{ get;set;}
+		
+		public string correoElectrónico { get;set;}
+		
+		public string direcciónEnvío { get;set;}
+		
+		public string fechaCompra {  get;set;}
+
+		[Key]
+		public int id { get;set;}
+
+		public string nombreCliente { get;set;}
+
+		public double precioTotal { get;set;}
+
+		public int teléfono { get;set;}
+
+	}
+}
