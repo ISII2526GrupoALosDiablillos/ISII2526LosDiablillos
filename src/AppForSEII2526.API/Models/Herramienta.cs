92f88ab04a82104d47e2de5e434e5fbab3a03ea1
ÍKER VALÍA GARCÍA <Iker.Valia@alu.uclm.es>
2025-10-04 18:32:07 +0200

    #6 Primera version de clase Herramienta

---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Herramienta
+    {
+        public int compraItems {  get; set; }
+        [Key]
+        public int id {  get; set; }
+        public int itemsReparación {  get; set; }
+        public string material { get; set; }
+        public string nombre {  get; set; }
+        public int OfertaItems {  get; set; }
+        public int precio {  get; set; }
+        public int tiempoReparacion {  get; set; }
+
+
+    }
+}
