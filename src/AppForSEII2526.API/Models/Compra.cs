14cee478603e72e56c216545490f9314d1b199b9
Gonzalo <Gonzalo.Ortiz@alu.uclm.es>
2025-10-05 19:45:46 +0200

    #9 Clase Compra

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -3,14 +3,19 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Compra
 {
-	public String apellidoCliente {  get; set; }
-	public String correoElectronico { get; set; }
-	public String direccionEnvio { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su apellido.")]
+    public String apellidoCliente {  get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su correo electrónico.")]
+    public String correoElectronico { get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su dirección")]
+    public String direccionEnvio { get; set; }
 	public DateTime fechaCompra {  get; set; }
 	public int id {  get; set; }
+	[Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su nombre.")]
 	public String nombreCliente { get; set; }
 	public double preciototal { get; set; }
-	public int telefono { get; set; }
+    public int telefono { get; set; }
 	public tiposMetodoPago metodoPago { get; set; }
 
 	public enum tiposMetodoPago
