50a5daa05caeb8a1984d2a942071740bd3f5d294
GONZALO ORTIZ MUÑOZ <Gonzalo.Ortiz@alu.uclm.es>
2025-10-06 10:32:13 +0200

    #38 Clase "Compra" actualizada.

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -5,22 +5,46 @@ public class Compra
 {
     
     [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su apellido.")]
-    public String apellidoCliente {  get; set; }
+    public String ApellidoCliente {  get; set; }
     [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su correo electrónico.")]
-    public String correoElectronico { get; set; }
+    public String CorreoElectronico { get; set; }
     [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su dirección")]
-    public String direccionEnvio { get; set; }
-	public DateTime fechaCompra {  get; set; }
-	public int id {  get; set; }
+    public String DireccionEnvio { get; set; }
+	public DateTime FechaCompra {  get; set; }
+	public int Id {  get; set; }
 	[Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su nombre.")]
-	public String nombreCliente { get; set; }
-	public double preciototal { get; set; }
-    public int telefono { get; set; }
-
-	public Lista<CompraItem> ComprasItem { get; set; }
-
+	public String NombreCliente { get; set; }
+	public double Preciototal { get; set; }
+    public int Telefono { get; set; }
+	public List<CompraItem> ComprasItem { get; set; }
+	public Compra() { }
+	public Compra(String apellidoCliente, String correoElectronico, String direccionEnvio, DateTime fechaCompra, int id, String nombreCliente, double preciototal, int telefono)
+	{
+		ApellidoCliente = apellidoCliente;
+		CorreoElectronico = correoElectronico;
+		DireccionEnvio = direccionEnvio;
+		FechaCompra = fechaCompra;
+		Id = id;
+		NombreCliente = nombreCliente;
+		Preciototal = preciototal;
+		Telefono = telefono;
+	}
+	public String telefono;
+	public String direccionEnvio;
+	public String apellidoCliente;
+	public String correoElectronico;
+	public DateTime fechaCompra;
+	public String nombreCliente;
+	public double preciototal;
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 	public tiposMetodoPago metodoPago { get; set; }
-
 	public enum tiposMetodoPago
 	{
 		TarjetaCredito,
