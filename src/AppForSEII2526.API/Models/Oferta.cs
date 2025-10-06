f1771e6de430354db76fbfc9ab7fc7c1afa68438
AlejandroOrtega6 <alejandro.ortega6@alu.uclm.es>
2025-10-06 09:36:40 +0200

    Restricciones actualizadas

--- src/AppForSEII2526.API/Models/Oferta.cs
+++ src/AppForSEII2526.API/Models/Oferta.cs
@@ -5,8 +5,9 @@
         public DateTime fechaFinal {  get; set; }
         public DateTime fechaInicio { get; set; }
         public DateTime fechaOferta { get; set; }
+        [Key]
         public int Id { get; set; }
-        
+        [Required(ErrorMessage = "El método de pago es obligatorio.")]
         public tiposMetodoPago metodoPago { get; set; }
         public enum tiposMetodoPago
         {
@@ -14,7 +15,7 @@
             PayPal,
             Efectivo
         }
-        
+        [Required(ErrorMessage = "Debe indicar a quién va dirigida la oferta.")]
         public tiposDirigidaOferta dirigidaOferta { get; set; }
         public enum tiposDirigidaOferta
         {
