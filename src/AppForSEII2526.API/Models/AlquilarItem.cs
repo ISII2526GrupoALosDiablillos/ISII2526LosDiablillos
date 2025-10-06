22d237e98f5c07a5594e5d2e5a88c14cab743f5c
ÍKER VALÍA GARCÍA <Iker.Valia@alu.uclm.es>
2025-10-06 10:47:18 +0200

    #37 Añado todos los cambios y arreglo codigo

--- src/AppForSEII2526.API/Models/AlquilarItem.cs
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -10,6 +10,34 @@
         public int precio {  set; get; }
         public int cantidad {  set; get; }
 
+        public AlquilarItem()
+        {
 
+        }
+
+        public AlquilarItem(int idAlquiler,Herramienta herramienta,Alquilar alquilar,int idHerramienta, int precio, int cantidad)
+        {
+            IdAlquiler = idAlquiler;
+            Herramienta= herramienta;
+            Alquilar = alquilar;
+            IdHerramienta = idHerramienta;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
+        public int IdAlquiler { get; set; }
+        public Herramienta Herramienta { get; set; }
+        public Alquilar Alquilar { get;set; }
+        public int IdHerramienta { get; set; }
+        public int Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
