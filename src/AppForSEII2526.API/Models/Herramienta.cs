77136095220388492fac19e482b8126ad836099a
MIGUEL ENDRINO GONZÁLEZ <Miguel.Endrino@alu.uclm.es>
2025-10-06 18:02:22 +0200

    #69 Anadir Ilist en Herramienta

--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -6,7 +6,7 @@
         [Key]
         public int id {  get; set; }
         [StringLength(100, ErrorMessage = "El nombre no puede tener mas de 100 caracteres")]
-        public int itemsReparación {  get; set; }
+        public int itemsReparacion {  get; set; }
         public string material { get; set; }
         [StringLength(100, ErrorMessage ="El nombre no puede tener mas de 100 caracteres")]
 
@@ -22,6 +22,37 @@
         public Fabricante Fabricante { get; set; }
         public IList<AlquilarItem> alquilarItems { get; set; }
         public IList<OfertaItem> ofertaItems { get; set; }
+
         public IList<ReparacionItem> reparacionItems { get; set; }
+
+        public Herramienta() { }
+        public Herramienta(int compraItems,int id,int itemsReparación,string material,string nombre,int precio,int tiempoReparacion)
+        {
+            CompraItem=compraItems;
+            Id = id;
+            ItemsReparacion = itemsReparacion;
+            Material = material;
+            Nombre = nombre;
+            Precio = precio;
+            TiempoReparacion = tiempoReparacion;
+
         }
+        public int Id {  get; set; }
+        public int CompraItem { get; set; }
+        public int ItemsReparacion { get;set; }
+        public string Material { get; set; }
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public int TiempoReparacion { get; set; }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    
+
 }
