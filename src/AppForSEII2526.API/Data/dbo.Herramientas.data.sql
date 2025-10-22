0891f6548aa18e1920b7869a883ad43e20e77d6d
ÍKER VALÍA GARCÍA <Iker.Valia@alu.uclm.es>
2025-10-22 12:36:48 +0200

    #106

---
+++ src/AppForSEII2526.API/Data/dbo.Herramientas.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Herramientas] ON
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (1, 1, N'madera', N'Juan', 1, 1, 1, 1)
+SET IDENTITY_INSERT [dbo].[Herramientas] OFF
