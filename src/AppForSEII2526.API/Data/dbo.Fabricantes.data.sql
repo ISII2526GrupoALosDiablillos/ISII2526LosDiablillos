542d415f90adf331ee6400c374488a1006858454
AlejandroOrtega6 <alejandro.ortega6@alu.uclm.es>
2025-10-22 12:21:33 +0200

    #104 Creado controller y dto para mi CU

---
+++ src/AppForSEII2526.API/Data/dbo.Fabricantes.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Fabricantes] ON
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (1, N'Pepe')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (2, N'Juan')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (3, N'Sonia')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (4, N'Julio')
+SET IDENTITY_INSERT [dbo].[Fabricantes] OFF
---
+++ src/AppForSEII2526.API/Data/dbo.Herramientas.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Herramientas] ON
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (3, 12, N'Madera', N'Pepe', 3, 70, 100, 1)
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (4, 5, N'Plastico', N'Jose', 6, 120, 60, 2)
+SET IDENTITY_INSERT [dbo].[Herramientas] OFF
