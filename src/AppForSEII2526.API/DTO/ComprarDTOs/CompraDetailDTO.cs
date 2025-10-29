344b27bb788aabf6604f4e41a5fe9f11b2f87638
GONZALO ORTIZ MUÑOZ <Gonzalo.Ortiz@alu.uclm.es>
2025-10-29 12:10:51 +0100

    #128 Controller para Comprar corregido.

---
+++ src/AppForSEII2526.API/DTO/ComprarDTOs/CompraDetailDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CompraDetailDTO
+    {
+        public String nombre_cliente { get; set; }
+        public String apellido_cliente { get; set; }
+        public String direccion { get; set; }
+        public double preciototal { get; set; }
+        public DateTime fechaCompra { get; set; }
+        public CompraDetailDTO() { }
+        public CompraDetailDTO(String nombre_cliente, String apellido_cliente, String direccion, double preciototal, DateTime fechaCompra)
+        {
+            this.nombre_cliente = nombre_cliente;
+            this.apellido_cliente = apellido_cliente;
+            this.direccion = direccion;
+            this.preciototal = preciototal;
+            this.fechaCompra = fechaCompra;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraDetailDTO dTO &&
+                   nombre_cliente == dTO.nombre_cliente &&
+                   apellido_cliente == dTO.apellido_cliente &&
+                   direccion == dTO.direccion &&
+                   preciototal == dTO.preciototal &&
+                   fechaCompra == dTO.fechaCompra;
+                   
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre_cliente, apellido_cliente, direccion, preciototal, fechaCompra);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTO/ComprarDTOs/CompraItemDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CompraItemDTO
+    {
+        public string nombre { get; set; }
+        public string material { get; set; }
+        public decimal precio { get; set; }
+        public string descripcion { get; set; }
+        public int cantidad { get; set; }
+        public CompraItemDTO() { }
+        public CompraItemDTO(string nombre, string material, decimal precio, string descripcion, int cantidad)
+        {
+            this.nombre = nombre;
+            this.material = material;
+            this.precio = precio;
+            this.descripcion = descripcion;
+            this.cantidad = cantidad;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraItemDTO dTO &&
+                   nombre == dTO.nombre &&
+                   material == dTO.material &&
+                   precio == dTO.precio &&
+                   descripcion == dTO.descripcion &&
+                   cantidad == dTO.cantidad;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre, material, precio, descripcion, cantidad);
+        }
+    }
+}
