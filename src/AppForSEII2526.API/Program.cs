f0aefef2f203442d17088eab4719f05d601df837
AlejandroOrtega6 <alejandro.ortega6@alu.uclm.es>
2025-11-16 12:46:15 +0100

    borrados errores de program.cs

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -1,21 +1,19 @@
 using System.Reflection;
 using System.Text.Json.Serialization;
 using System.Data.Common;
-using TodoApi.Logging;
+
 
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
 
-using AppForSEII2526.API.Logging;
+
 using AppForSEII2526.API.Data;
 using AppForSEII2526.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- Logging RabbitMQ ---
-builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
 
 // --- Add services to the container ---
 builder.Services.AddControllers()
@@ -46,14 +44,8 @@ switch (connection2Database)
             options.UseSqlServer(connectionString));
         break;
 }
-<<<<<<< HEAD
-builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
-//”RabbitMQ” coincide con el nombre del bloque de propiedades en appsettings.json
-//Add Identity services to the container
-=======
 
 // --- Identity ---
->>>>>>> origin/development
 builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
     .AddRoles<IdentityRole>()
@@ -85,25 +77,11 @@ var app = builder.Build();
 var startupLogger = app.Services.GetRequiredService<ILogger<Program>>();
 startupLogger.LogInformation("API arrancada en {Env} a {Time}", app.Environment.EnvironmentName, DateTime.UtcNow);
 
-<<<<<<< HEAD
-
-
-//Map Identity routes
-//app.MapIdentityApi<IdentityUser>();
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-
-
-
-using (var scope = app.Services.CreateScope()) {
-    try {
-
-=======
 // --- Inicialización BD ---
 using (var scope = app.Services.CreateScope())
 {
     try
     {
->>>>>>> origin/development
         var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         if (connection2Database == "SQLite")
