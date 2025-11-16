473e9efda06cae5eca371457f65da97844860586
Gonzalo <Gonzalo.Ortiz@alu.uclm.es>
2025-11-16 23:49:24 +0100

    #184 BASE DE DATOS DE COMPRAR HERRAMIENTAS CON SUS DATOS.

---
+++ src/AppForSEII2526.API/Data/dbo.Fabricantes.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Fabricantes] ON
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (1, N'Manolo')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (2, N'Estefania')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (3, N'Manolita')
+SET IDENTITY_INSERT [dbo].[Fabricantes] OFF
---
+++ src/AppForSEII2526.API/Data/dbo.Herramientas.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Herramientas] ON
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (2, 3, N'Aluminio', N'Maza', 2, 30, 4, 1)
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (5, 4, N'Madera', N'Martillo', 5, 20, 2, 2)
+INSERT INTO [dbo].[Herramientas] ([id], [itemsReparacion], [material], [nombre], [OfertaItems], [precio], [tiempoReparacion], [fabricanteId]) VALUES (6, 5, N'Metal', N'Destornillador', 3, 27, 3, 3)
+SET IDENTITY_INSERT [dbo].[Herramientas] OFF
