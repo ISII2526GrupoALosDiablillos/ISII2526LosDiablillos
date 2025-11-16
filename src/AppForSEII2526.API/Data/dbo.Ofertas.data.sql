f26d64d711d6124e7314d10602a81d8465c89dbd
eltrolleralex <alex10305007@gmail.com>
2025-11-16 23:34:08 +0100

    #183 BASES DE DATOS CREADAS

---
+++ src/AppForSEII2526.API/Data/dbo.Ofertas.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Ofertas] ON
+INSERT INTO [dbo].[Ofertas] ([Id], [fechaFinal], [fechaInicio], [fechaOferta], [metodoPago], [dirigidaOferta], [applicationUserId]) VALUES (14, N'2026-03-01 00:00:00', N'2026-02-01 00:00:00', N'2026-01-20 00:00:00', 0, 0, N'1')
+SET IDENTITY_INSERT [dbo].[Ofertas] OFF
