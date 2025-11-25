8d27e53380346c272413f2b4d123fc0b3a195c2d
eltrolleralex <alex10305007@gmail.com>
2025-11-25 19:17:51 +0100

    StateContainer de Oferta creado

---
+++ src/AppForSEII2526.Web/OfertaStateContainer.cs
@@ -0,0 +1,55 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class OfertaStateContainer
+    {
+        public OfertaForCreateDTO Oferta { get; private set; } = new OfertaForCreateDTO()
+        {
+            OfertaItems = new List<OfertaItemForCreateDTO>()
+        };
+
+        public float PrecioTotal
+        {
+            get
+            {
+                return (float)Oferta.OfertaItems.Sum(oi => oi.PrecioFinal);
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddHerramientaToOferta(HerramientaParaOfertarDTO herramienta, double porcentaje)
+        {
+            if (!Oferta.OfertaItems.Any(oi => oi.HerramientaId == herramienta.Id))
+                Oferta.OfertaItems.Add(new OfertaItemForCreateDTO()
+                {
+                    HerramientaId = herramienta.Id,
+                    Porcentaje = porcentaje,
+                    PrecioFinal = herramienta.Precio - (herramienta.Precio * (porcentaje / 100.0))
+                }
+            );
+        }
+
+        public void RemoveOfertaItemToOferta(OfertaItemForCreateDTO item)
+        {
+            Oferta.OfertaItems.Remove(item);
+        }
+
+        public void ClearOfertaCart()
+        {
+            Oferta.OfertaItems.Clear();
+        }
+
+        public void OfertaProcessed()
+        {
+            Oferta = new OfertaForCreateDTO()
+            {
+                OfertaItems = new List<OfertaItemForCreateDTO>()
+            };
+        }
+    }
+}
